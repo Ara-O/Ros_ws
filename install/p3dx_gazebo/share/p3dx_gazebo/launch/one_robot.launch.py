130a8ab45568a1031a45384f928fdb5d9b8f054b
Ara-O <oladipoeyiara@gmail.com>
2024-05-29 10:50:28 -0400

    more cnhanges

--- install/p3dx_gazebo/share/p3dx_gazebo/launch/one_robot.launch.py
+++ install/p3dx_gazebo/share/p3dx_gazebo/launch/one_robot.launch.py
@@ -1,9 +1,8 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
@@ -28,7 +27,7 @@ def generate_launch_description():
             default_value=os.path.join(pkg_p3dx_description, 'urdf', 'pioneer3dx.xacro'),
             description='Path to the robot URDF file'
         ),
-        
+
         # Load the URDF into the parameter server
         Node(
             package='robot_state_publisher',
@@ -39,16 +38,30 @@ def generate_launch_description():
                 'robot_description': Command(['xacro ', LaunchConfiguration('model')])
             }]
         ),
-        
+
+        OpaqueFunction(function=spawn_robot),
+    ])
+
+def spawn_robot(context, *args, **kwargs):
+    # Get the initialization pose
+    init_pose = LaunchConfiguration('init_pose').perform(context).split()
+    
+    # Extract x, y, z values
+    x = init_pose[1]
+    y = init_pose[3]
+    z = init_pose[5]
+
+    return [
         # Spawn the robot in Gazebo
         Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
             name='urdf_spawner',
             output='screen',
-            arguments=['-entity', LaunchConfiguration('robot_name'), '-topic', 'robot_description', LaunchConfiguration('init_pose')]
+            arguments=['-entity', LaunchConfiguration('robot_name').perform(context), '-topic', 'robot_description',
+                       '-x', x, '-y', y, '-z', z]
         ),
-        
+
         # Joint State Publisher
         Node(
             package='joint_state_publisher',
@@ -57,12 +70,7 @@ def generate_launch_description():
             parameters=[{'use_gui': False}],
             remappings=[('/robot_description', 'robot_description')]
         ),
-        
-        # Robot State Publisher
-        #Node(
-        #     package='robot_state_publisher',
-        #     executable='robot_state_publisher',
-        #     name='robot_state_publisher',
-        #     output='screen'
-        # ),
-    ])
+    ]
+
+if __name__ == '__main__':
+    generate_launch_description()
